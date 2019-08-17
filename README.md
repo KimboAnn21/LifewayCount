3aac4c2a04679694a5604452d2f1acfb0f2254b8
[Kim Grossman] <[kimgrossman21@gmail.com]>
2019-08-16 21:09:00 -0600

    Coding Challenge

---
+++ README.md
@@ -0,0 +1,4 @@
+How to RUN:  
+1. Clone repository into code editor.  
+2. Inside cd lifewayCount do an NPM install from the command line. 
+3. 
---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const PORT = 3000;
+
+const app = express; 
+
+app.use(express.json());
+
+//key will be the id, value: message
+let messages = {};
+let count = 0;
+app.post("/wordcount", (req, res) => {
+    console.log(req.body);
+
+    if(messages[req.body.id] !== undefined) {
+        res.send(`ID: ${req.body.id} is already being used.`);
+        return;
+    }
+
+    messages[req.body.id] = req.body.message;
+
+    const words = req.body.message.split(" ");
+    const wordCount = words.length;
+    count += wordCount;
+
+    console.log(words);
+
+    res.json({
+        count: count
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`App is running on ${PORT}`)
+})
